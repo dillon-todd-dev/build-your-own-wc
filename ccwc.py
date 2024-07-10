27c3d576254d05fe378e80d8320232c3cef7f5da
dillon-todd-dev <dillontodd.dev@gmail.com>
2024-07-10 17:16:36 -0500

    add option to count the number of words

--- ccwc.py
+++ ccwc.py
@@ -4,6 +4,7 @@ import os
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--count', action='store_true', help='count the number of bytes in the file')
 parser.add_argument('-l', '--length', action='store_true', help='count the number of lines in the file')
+parser.add_argument('-w', '--words', action='store_true', help='count the number of words in the file')
 parser.add_argument('file_path')
 args = parser.parse_args()
 
@@ -15,3 +16,10 @@ if args.length:
     with open(args.file_path) as fp:
         num_lines = len(fp.readlines())
         print(num_lines, args.file_path)
+
+if args.words:
+    with open(args.file_path) as fp:
+        data = fp.read()
+        lines = data.split()
+        num_words = len(lines)
+        print(num_words, args.file_path)
