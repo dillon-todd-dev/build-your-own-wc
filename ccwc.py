8d18d5813be8af1b2e6ecf6dd63b8c4c3140fbb1
dillon-todd-dev <dillontodd.dev@gmail.com>
2024-07-10 17:12:22 -0500

    add option to count number of lines

--- ccwc.py
+++ ccwc.py
@@ -3,9 +3,15 @@ import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--count', action='store_true', help='count the number of bytes in the file')
+parser.add_argument('-l', '--length', action='store_true', help='count the number of lines in the file')
 parser.add_argument('file_path')
 args = parser.parse_args()
 
 if args.count:
     file_size = os.stat(args.file_path)
     print(file_size.st_size, args.file_path)
+
+if args.length:
+    with open(args.file_path) as fp:
+        num_lines = len(fp.readlines())
+        print(num_lines, args.file_path)
